f05ccc55677554e29d6e5f8d2bba0b84fe99fbaa
Kushal HK <115166395+Kushalhk@users.noreply.github.com>
2023-12-27 13:05:49 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1417,7 +1417,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text="◆◆◇"
+            text="◆◇◇"
         )
         await query.message.edit_text(
             text="◆◆◇"
