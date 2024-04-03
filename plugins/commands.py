a6e92eaee519bfcb63d2026ef399100ada2e925f
Kushal HK <115166395+Kushalhk@users.noreply.github.com>
2024-04-03 19:23:46 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -262,9 +262,9 @@ async def start(client, message):
         k = await client.send_message(chat_id=message.from_user.id,text=f"<b>Get All Files in a Single Click!!!\n\n📂 ʟɪɴᴋ ➠ : {g}\n\n<i>Note: This message is deleted in 5 mins to avoid copyrights. Save the link to Somewhere else</i></b>", reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton('🔁 𝐆𝐄𝐓 𝐅𝐈𝐋𝐄 🔁', url=g)
+                        InlineKeyboardButton('🔰 ɢᴇᴛ ғɪʟᴇ 🔰', url=g)
                     ], [
-                        InlineKeyboardButton('⚜️ Hᴏᴡ Tᴏ Dᴏᴡɴʟᴏᴀᴅ ⚜️', url=await get_tutorial(chat_id))
+                        InlineKeyboardButton('⚜️ ʜᴏᴡ ᴛᴏ ɢᴇᴛ ⚜️', url=await get_tutorial(chat_id))
                     ]
                 ]
             )
@@ -283,9 +283,9 @@ async def start(client, message):
         k = await client.send_message(chat_id=user,text=f"<b>📕Nᴀᴍᴇ ➠ : <code>{files.file_name}</code> \n\n🔗Sɪᴢᴇ ➠ : {get_size(files.file_size)}\n\n📂Fɪʟᴇ ʟɪɴᴋ ➠ : {g}\n\n<i>Note: This message is deleted in 20 mins to avoid copyrights. Save the link to Somewhere else</i></b>", reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton('🔁 𝐆𝐄𝐓 𝐅𝐈𝐋𝐄 🔁', url=g)
+                        InlineKeyboardButton('🔰 ɢᴇᴛ ғɪʟᴇ 🔰', url=g)
                     ], [
-                        InlineKeyboardButton('⚜️ Hᴏᴡ Tᴏ Dᴏᴡɴʟᴏᴀᴅ ⚜️', url=await get_tutorial(chat_id))
+                        InlineKeyboardButton('⚜️ ʜᴏᴡ ᴛᴏ ɢᴇᴛ ⚜️', url=await get_tutorial(chat_id))
                     ]
                 ]
             )
@@ -364,9 +364,9 @@ async def start(client, message):
             k = await client.send_message(chat_id=message.from_user.id,text=f"<b>📕Nᴀᴍᴇ ➠ : <code>{files.file_name}</code> \n\n🔗Sɪᴢᴇ ➠ : {get_size(files.file_size)}\n\n📂Fɪʟᴇ ʟɪɴᴋ ➠ : {g}\n\n<i>Note: This message is deleted in 20 mins to avoid copyrights. Save the link to Somewhere else</i></b>", reply_markup=InlineKeyboardMarkup(
                     [
                         [
-                            InlineKeyboardButton('🔁 𝐆𝐄𝐓 𝐅𝐈𝐋𝐄 🔁', url=g)
+                            InlineKeyboardButton('🔰 ɢᴇᴛ ғɪʟᴇ 🔰', url=g)
                         ], [
-                            InlineKeyboardButton('⚜️ Hᴏᴡ Tᴏ Dᴏᴡɴʟᴏᴀᴅ ⚜️', url=await get_tutorial(chat_id))
+                            InlineKeyboardButton('⚜️ ʜᴏᴡ ᴛᴏ ɢᴇᴛ ⚜️', url=await get_tutorial(chat_id))
                         ]
                     ]
                 )
